3d0644639069f5a8c8f641e8b13d59232ef28cbc
Burak <14030652+BurakAy@users.noreply.github.com>
2023-01-28 18:33:26 -0600

    hero component and styles

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import "../styles/Hero.css";
+import groupimage from "../assets/airbnb_group.png";
+
+const Hero = () => {
+  return (
+    <div className="hero--container">
+      <img
+        src={groupimage}
+        alt="Airbnb group of images"
+        className="hero--image"
+      />
+      <div className="hero--text">
+        <h1>Online Experiences</h1>
+        <p>
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
