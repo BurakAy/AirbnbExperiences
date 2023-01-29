ad115ac03346623202f861544684d5a82a76e300
Burak <14030652+BurakAy@users.noreply.github.com>
2023-01-28 19:16:32 -0600

    card component and styling

---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import "../styles/Card.css";
+import katieimage from "../assets/katie_z.png";
+import staricon from "../assets/star.png";
+
+const Card = () => {
+  return (
+    <div className="card--container">
+      <div className="card--image_wrapper">
+        <span className="card--image_label"></span>
+        <img src={katieimage} alt="Katie Zaferes" className="card--image" />
+      </div>
+      <div className="card--details">
+        <p className="card--rating">
+          <img src={staricon} alt="star rating" className="card--star_icon" />
+          <span className="card--rating_text">5.0&nbsp;</span>
+          <span className="card--rating_count">(6) &bull; USA</span>
+        </p>
+        <p className="card--title">Life lessons with Katie Zaferes</p>
+        <p className="card--pricing">
+          <strong>From $136</strong> / person
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
